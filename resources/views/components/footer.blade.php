76e78cd707840ea1e75bdb849d3396256c7f715f
Stefano Scarpa <scarpastefano@icloud.com>
2024-08-04 19:48:57 +0200

    change icon tecnologies, color icon, edit ui

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,12 +1,12 @@
 
 
-<footer class="bg-white dark:bg-gray-900">
+<footer class="bg-gray-900 py-16">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
               <a href="https://flowbite.com/" class="flex items-center">
-                  <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" />
-                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                  {{-- <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" /> --}}
+                  <span class="self-center text-2xl font-semibold whitespace-nowrap text-gray-50">Stefano Scarpa</span>
               </a>
           </div>
           <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
@@ -47,7 +47,7 @@
       </div>
       <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <div class="sm:flex sm:items-center sm:justify-between">
-          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="{{route('homepage')}}" class="hover:underline">Stefano Scarpa</a>. All Rights Reserved.
           </span>
           <div class="flex mt-4 sm:justify-center sm:mt-0">
               <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,12 +1,12 @@
-<nav class="bg-gray-900 border-dark-800 dark:bg-dark-900 container-none p-3 m-0 fixed top-0 w-full z-50">
+<nav class="bg-transparent border-dark-800 dark:bg-dark-900 container-none p-3 m-0 fixed top-0 w-full z-50 backdrop-blur-sm">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
     <a href="{{route('homepage')}}" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <span class="self-center text-2xl font-semibold whitespace-nowrap text-gray-50">Stefano Scarpa</span>
+        <span class="self-center text-2xl font-semibold whitespace-nowrap text-gray-50"><span class="text-orange-500">Stefano</span>Scarpa</span>
     </a>
     <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
 
       <a href="{{route('contacts')}}">
-        <button type="button" class="border-0 text-white bg-orange-500 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm px-4 py-2 text-center dark:bg-orange-500 dark:hover:bg-orange-500 dark:focus:ring-orange-500">Contattami</button>
+        <button type="button" class="border-0 text-white bg-orange-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-md px-4 py-2 text-center dark:bg-orange-500 dark:focus:ring-orange-500">Contattami</button>
       </a>
 
         <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700" aria-controls="navbar-cta" aria-expanded="false">
@@ -20,16 +20,16 @@
     <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
       <ul class="flex flex-col font-medium md:p-0 border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row mt-0 md:mt-0 md:border-0 ">
         <li>
-          <a href="{{route('homepage')}}" class="block py-2 px-3 md:p-0 text-gray-100 rounded md:hover:bg-transparent md:hover:text-orange-500 md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</a>
+          <a href="{{route('homepage')}}" class="block link-menu text-lg font-thin py-2 px-3 md:p-0 text-gray-100 rounded md:hover:bg-transparent md:hover:text-orange-500 md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</a>
         </li>
         <li>
-          <a href="{{route('aboutme')}}" class="block py-2 px-3 md:p-0 text-gray-100 rounded md:hover:bg-transparent md:hover:text-orange-500 md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About me</a>
+          <a href="{{route('aboutme')}}" class="block link-menu text-lg font-thin py-2 px-3 md:p-0 text-gray-100 rounded md:hover:bg-transparent md:hover:text-orange-500 md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About me</a>
         </li>
         <li>
-          <a href="#" class="block py-2 px-3 md:p-0 text-gray-100 rounded md:hover:bg-transparent md:hover:text-orange-500 md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Skills</a>
+          <a href="#" class="block link-menu text-lg font-thin py-2 px-3 md:p-0 text-gray-100 rounded md:hover:bg-transparent md:hover:text-orange-500 md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Skills</a>
         </li>
         <li>
-          <a href="{{route('contacts')}}" class="block py-2 px-3 md:p-0 text-gray-100 rounded md:hover:bg-transparent md:hover:text-orange-500 md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+          <a href="{{route('contacts')}}" class="block link-menu text-lg font-thin py-2 px-3 md:p-0 text-gray-100 rounded md:hover:bg-transparent md:hover:text-orange-500 md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
         </li>
       </ul>
     </div>
