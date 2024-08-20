017fe45eabc53d9af1a65ca931acf90436ce2994
Stefano Scarpa <scarpastefano@icloud.com>
2024-08-20 17:25:49 +0200

    fix

--- deploy.php
+++ deploy.php
@@ -20,3 +20,89 @@ host('195.231.86.127')
 // Hooks
 
 after('deploy:failed', 'deploy:unlock');
+
+
+
+// <?php
+// namespace Deployer;
+// require 'recipe/laravel.php';
+
+// // Configuration
+// set('keep_releases', 5);
+// set('ssh_type', 'native');
+// set('ssh_multiplexing', true);
+// set('branch', 'master');
+// set('repository', 'git@ip:root/~/temp/repo.git');
+
+// add('shared_files', [
+//     '.env'
+// ]);
+// add('shared_dirs', [
+//     'storage'
+// ]);
+
+// add('writable_dirs', [
+//     'bootstrap/cache',
+//     'storage',
+//     'storage/app',
+//     'storage/app/public',
+//     'storage/framework',
+//     'storage/framework/cache',
+//     'storage/framework/sessions',
+//     'storage/framework/views',
+//     'storage/logs',
+// ]);
+
+// // Servers
+
+// server('production', 'ip')
+//     ->user('root')
+//     ->identityFile()
+//     ->configFile('~/.ssh/config')
+//     ->set('deploy_path', '/var/www/domain')
+//     ->pty(true);
+
+
+// // Tasks
+
+// task('upload:env', function () {
+//     upload('.env.production', '{{deploy_path}}/shared/.env');
+// })->desc('Environment setup');
+
+
+// desc('Restart PHP-FPM service');
+// task('php-fpm:restart', function () {
+//     // The user must have rights for restart service
+//     // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
+//     run('sudo systemctl restart php-fpm.service');
+// });
+// after('deploy:symlink', 'php-fpm:restart');
+
+// // [Optional] if deploy fails automatically unlock.
+// after('deploy:failed', 'deploy:unlock');
+
+// // Migrate database before symlink new release.
+// desc('Deploy your project');
+// task('deploy', [
+//     'deploy:prepare',
+//     'deploy:lock',
+//     'deploy:release',
+//     'deploy:update_code',
+//     'deploy:shared',
+//     'upload:env',
+//     'deploy:vendors',
+//     'deploy:writable',
+//     'artisan:migrate',
+//     'artisan:view:clear',
+//     'artisan:cache:clear',
+//     'artisan:config:cache',
+//     'artisan:route:cache',
+//     'artisan:optimize',
+//     'deploy:symlink',
+//     'deploy:unlock',
+//     'cleanup',
+// ]);
+
+
+
+// before('deploy:symlink', 'artisan:migrate');
