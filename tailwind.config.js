5224891654cb1f5a040e0a85a840cab1c8fa7d8e
Stefano Scarpa <scarpastefano@icloud.com>
2024-08-29 16:21:28 +0200

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,12 @@ export default {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
-    "./resources/**/*.vue"
+    "./resources/**/*.vue",
+    "./resources/**/*.css"
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    
-  ],
+  plugins: [],
 }
 
