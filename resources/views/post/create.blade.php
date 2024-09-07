6e576acaa6b94bfc171f30e77f12c3ba58a8cdcb
Stefano Scarpa <scarpastefano@icloud.com>
2024-09-07 14:22:19 +0200

    fix

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -29,7 +29,7 @@
                     <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
                     <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
                 </div>
-                <input id="image" name="image" type="file" class="" />
+                <input id="image" name="image" multiple type="file" class="" />
             </label>
         </div> 
         @error('image')
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -5,7 +5,7 @@
         <div class="container-sm">
             <h1 class="text-gray-50 my-12 h2 font-bold">{{ $post->title }}</h1>
             <div class="grid grid-cols-1 sm:grid-cols-12 gap-5">
-                <div class="col-span-7">
+                <div class="col-span-7"> 
                     <img src="{{ Storage::url($post->image) }}" alt="nessuna immagine">
                 </div>
                 <div class="col-span-5">
